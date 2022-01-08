26f385058f989cee5d6d191903a591ceb6c0b066
HDL951236874 <2862308357@qq.com>
2022-01-08 06:34:07 -0500

    2022.1.3 Praticing the linked list problem.

--- src/LinkedLIst/Problem25.java
+++ src/LinkedLIst/Problem25.java
@@ -1,5 +1,6 @@
 package LinkedLIst;
 
+// queue can be implemented by the linkedList
 public class Problem25 {
     public ListNode reverseKGroup(ListNode head, int k) {
         ListNode pointer = head;
@@ -7,17 +8,36 @@ public class Problem25 {
         while (flag == 1) {
             //this is the judgement
             ListNode temp = head;
+            ListNode inverse = new ListNode(0);
             for (int i = 0; i < k; i++) {
-                if (temp.next == null) {
+                if (temp == null) {
                     flag = 0;
                     break;
                 } else {
+                    ListNode newList = new ListNode(temp.val);
+                    temp = temp.next;
+                    newList.next = inverse.next;
+                    inverse.next = newList;
 
                 }
             }
-            //this is the main function
+            if (flag != 0) {
+                inverse = inverse.next;
+                while (inverse != null) {
+                    head.val = inverse.val;
+                    head = head.next;
+                    inverse = inverse.next;
+                }
+            }
         }
-        return new ListNode(0);
+        return pointer;
+    }
+
+    public static void main(String[] args) {
+        Problem25 p = new Problem25();
+        ListNode input = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4))));
+        ListNode res = p.reverseKGroup(input,2);
+        res.print();
     }
 }
 
