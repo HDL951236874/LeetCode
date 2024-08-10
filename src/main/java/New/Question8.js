21f07954565194f6af72d3be19af6f6d372304c2
HDL951236874 <2862308357@qq.com>
2024-08-10 18:17:35 -0400

    Question8.js

---
+++ src/main/java/New/Question8.js
@@ -0,0 +1,9 @@
+let myAtoi = function(s) {
+    const ans = Number.parseInt(s);
+    if (ans) {
+        if (ans <= -2147483648)return -2147483648;
+        else if (ans >= 2147483647)return 2147483647;
+        else return ans;
+    }
+    return 0;
+};
